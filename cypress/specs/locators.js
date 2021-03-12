a6b24652cdaac1a24cd59531c1d31097f6619ae7
paulcrisan <paul@overpass.com>
2021-03-12 12:55:11 +0200

    initial commit

---
+++ cypress/specs/locators.js
@@ -0,0 +1,19 @@
+const TestLocators = {
+    
+    "nextButton": '[id="jfCard-welcome-start"]',
+    "firstNameInput": '[data-component="first"]',
+    "lastNameInput": '[data-component="last"]',
+    "firstPageNextButton": '[data-type="control_fullname"] [aria-label="Next"]',
+    "secondPageNextButton": '[data-type="control_fileupload"] [aria-label="Next"]',
+    "uploadFile": '[type="file"]',
+    "signatureInput": '[data-component="signature"]',
+    "thirdPageNextButton": '[data-type="control_signature"] [aria-label="Next"]',
+    "dateInput": '[data-format="yyyymmdd"]',
+    "forthPageNextButton": '[data-type="control_datetime"] [aria-label="Next"]',
+    "securityQuestionDropdown": '[data-component="mixed-dropdown"]',
+    "securityQuestion": '[data-value="Name of the first pet"]',
+    'answerInput': '[data-type="text"]',
+    'submitButton': '[data-type="control_mixed"] [aria-label="Submit"]',
+};
+
+export { TestLocators };
